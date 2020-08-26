6f5f715a633a75fcdf7c7675fdd4e50dac9f6edc
jhanker <mohammadshahin460@gmail.com>
2020-08-26 07:44:12 +0300

    second commit

---
+++ src/component/Product/product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const product = () => {
+    
+    return (
+        <div>
+            <h3>This is product</h3>
+        </div>
+    );
+};
+
+export default product;
